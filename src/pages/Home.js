c8b604fdfee4dcb7eeb47ecfc73f020febfa816b
phurinat_sahapat <phurinat.b@sahapat.co.th>
2022-01-06 13:36:57 +0700

    remove dynamic title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -27,7 +27,6 @@ export default function Home() {
           smartBackspace: true 
         };
         typed.current = new Typed(el.current, options);
-        document.title = `Phurinat-${workHistory[currentIndex]}`
         return () => {
             typed.current.destroy();
         }
